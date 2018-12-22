10f942a82aea54e95f617049766de71249bae22e
asuforce <owata.sn@gmail.com>
2018-12-22 16:05:45 +0900

    Create server

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-
 	"github.com/asuforce/gin-gorm-tutorial/db"
+	"github.com/asuforce/gin-gorm-tutorial/server"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World")
-	})
-
 	db.Init()
-	r.Run()
+	server.Init()
 }
---
+++ server/server.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/asuforce/gin-gorm-tutorial/controller"
+)
+
+// Init is initialize server
+func Init() {
+	r := router()
+	r.Run()
+}
+
+func router() *gin.Engine {
+	r := gin.Default()
+
+	u := r.Group("/users")
+	{
+		ctrl := user.Controller{}
+		u.GET("", ctrl.Index)
+		u.GET("/:id", ctrl.Show)
+		u.POST("", ctrl.Create)
+		u.PUT("/:id", ctrl.Update)
+		u.DELETE("/:id", ctrl.Delete)
+	}
+
+	return r
+}
