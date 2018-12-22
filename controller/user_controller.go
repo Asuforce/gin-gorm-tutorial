4662dea1918d987f490ef27ec00037489b229bd1
asuforce <owata.sn@gmail.com>
2018-12-22 16:05:34 +0900

    Create User controller

---
+++ controller/user_controller.go
@@ -0,0 +1,70 @@
+package user
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/asuforce/gin-gorm-tutorial/service"
+)
+
+// Controller is user controlller
+type Controller struct{}
+
+// Index action: GET /users
+func (pc Controller) Index(c *gin.Context) {
+	var s user.Service
+	p, err := s.GetAll()
+	if err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	}
+	c.JSON(200, p)
+}
+
+// Create action: POST /users
+func (pc Controller) Create(c *gin.Context) {
+	var s user.Service
+	p, err := s.CreateModel(c)
+	if err != nil {
+		c.AbortWithStatus(400)
+		fmt.Println(err)
+	}
+	c.JSON(201, p)
+}
+
+// Show action: GET /users/:id
+func (pc Controller) Show(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s user.Service
+	p, err := s.GetByID(id)
+	if err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	}
+	c.JSON(200, p)
+}
+
+// Update action: PUT /users/:id
+func (pc Controller) Update(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s user.Service
+	p, err := s.UpdateByID(id, c)
+	if err != nil {
+		c.AbortWithStatus(400)
+		fmt.Println(err)
+	}
+	c.JSON(200, p)
+}
+
+// Delete action: DELETE /users/:id
+func (pc Controller) Delete(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s user.Service
+
+	if err := s.DeleteByID(id); err != nil {
+		c.AbortWithStatus(403)
+		fmt.Println(err)
+	}
+	c.JSON(204, gin.H{"id #" + id: "deleted"})
+}
