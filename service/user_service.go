eb578abd5b02f01374642ca7a8529e3e907dad1a
asuforce <owata.sn@gmail.com>
2018-12-22 16:05:22 +0900

    Create User service

---
+++ service/user_service.go
@@ -0,0 +1,70 @@
+package user
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/asuforce/gin-gorm-tutorial/db"
+	"github.com/asuforce/gin-gorm-tutorial/entity"
+)
+
+// Service procides user's behavior
+type Service struct{}
+
+// User is alias of entity.User struct
+type User entity.User
+
+// GetAll is get all User
+func (s Service) GetAll() ([]User, error) {
+	db := db.GetDB()
+	var u []User
+	if err := db.Find(&u).Error; err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+// CreateModel is create User model
+func (s Service) CreateModel(c *gin.Context) (User, error) {
+	db := db.GetDB()
+	var u User
+	c.BindJSON(u)
+
+	if err := db.Create(&u).Error; err != nil {
+		return u, err
+	}
+	return u, nil
+}
+
+// GetByID is get a User
+func (s Service) GetByID(id string) (User, error) {
+	db := db.GetDB()
+	var u User
+	if err := db.Where("id = ?", id).First(&u).Error; err != nil {
+		return u, err
+	}
+	return u, nil
+}
+
+// UpdateByID is update a User
+func (s Service) UpdateByID(id string, c *gin.Context) (User, error) {
+	db := db.GetDB()
+	var u User
+	if err := db.Where("id = ?", id).First(&u).Error; err != nil {
+		return u, err
+	}
+
+	c.BindJSON(u)
+	db.Save(u)
+
+	return u, nil
+}
+
+// DeleteByID is delete a User
+func (s Service) DeleteByID(id string) error {
+	db := db.GetDB()
+	var u User
+	if err := db.Where("id = ?", id).Delete(&u).Error; err != nil {
+		return err
+	}
+	return nil
+}
